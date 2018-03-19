0d5611998996f9e4f81485b3d15dac93032d552a
Hunter <huntergreer@gmail.com>
2018-03-19 12:06:42 -0500

    Answers and equation remain same through orientation shift

--- app/src/main/java/com/example/huntergreer/brainteaser/StartGameActivityFragment.java
+++ app/src/main/java/com/example/huntergreer/brainteaser/StartGameActivityFragment.java
@@ -25,6 +25,9 @@ import java.util.Random;
 public class StartGameActivityFragment extends Fragment {
     public static final String TAG = "StartGameActivityFragme";
 
+    public static final String CUR_EQUATION = "CurEquation";
+    public static final String CUR_SCORE = "CurScore";
+
     public static final String CUR_CHOICE_1 = "Choice1";
     public static final String CUR_CHOICE_2 = "Choice2";
     public static final String CUR_CHOICE_3 = "Choice3";
@@ -35,23 +38,16 @@ public class StartGameActivityFragment extends Fragment {
     public static final String CUR_CHOICE_3_TAG = "Choice3Tag";
     public static final String CUR_CHOICE_4_TAG = "Choice4Tag";
 
-    public static final String CUR_SCORE = "CurScore";
-
     public static final int INIT_SCORE = 0;
 
     public static final long TIMER_INTERVAL = 1000;
 
-    List<TextView> choices = new ArrayList<>();
+    List<TextView> choices;
 
     private TextView timer;
     private TextView equation;
     private TextView score;
     private TextView response;
-
-    private TextView choice1;
-    private TextView choice2;
-    private TextView choice3;
-    private TextView choice4;
 
     private long timerMillisLeft = 30000;
     private int answersCorrect;
@@ -138,10 +134,11 @@ public class StartGameActivityFragment extends Fragment {
         score = view.findViewById(R.id.score);
         response = view.findViewById(R.id.response);
 
-        choices.add((TextView)view.findViewById(R.id.choice1));
-        choices.add((TextView)view.findViewById(R.id.choice2));
-        choices.add((TextView)view.findViewById(R.id.choice3));
-        choices.add((TextView)view.findViewById(R.id.choice4));
+        choices = new ArrayList<>();
+        choices.add((TextView) view.findViewById(R.id.choice1));
+        choices.add((TextView) view.findViewById(R.id.choice2));
+        choices.add((TextView) view.findViewById(R.id.choice3));
+        choices.add((TextView) view.findViewById(R.id.choice4));
 
         return view;
     }
@@ -163,21 +160,27 @@ public class StartGameActivityFragment extends Fragment {
             Log.d(TAG, "playGame: savedInstanceState != null");
             Log.d(TAG, "playGame: choices.get(0).getText = " + choices.get(0).getText().toString());
 
-            choice1 = v.findViewById(R.id.choice1);
+            TextView choice1 = v.findViewById(R.id.choice1);
             choice1.setText(savedInstanceState.getString(CUR_CHOICE_1));
             choice1.setTag(savedInstanceState.getBoolean(CUR_CHOICE_1_TAG));
 
-            choice2 = v.findViewById(R.id.choice2);
+            TextView choice2 = v.findViewById(R.id.choice2);
             choice2.setText(savedInstanceState.getString(CUR_CHOICE_2));
             choice2.setTag(savedInstanceState.getBoolean(CUR_CHOICE_2_TAG));
 
-            choice3 = v.findViewById(R.id.choice3);
+            TextView choice3 = v.findViewById(R.id.choice3);
             choice3.setText(savedInstanceState.getString(CUR_CHOICE_3));
             choice3.setTag(savedInstanceState.getBoolean(CUR_CHOICE_3_TAG));
 
-            choice4 = v.findViewById(R.id.choice4);
+            TextView choice4 = v.findViewById(R.id.choice4);
             choice4.setText(savedInstanceState.getString(CUR_CHOICE_4));
             choice4.setTag(savedInstanceState.getBoolean(CUR_CHOICE_4_TAG));
+
+            TextView equation = v.findViewById(R.id.equation);
+            equation.setText(savedInstanceState.getString(CUR_EQUATION));
+
+            TextView score = v.findViewById(R.id.score);
+            score.setText(savedInstanceState.getString(CUR_SCORE));
         } else {
             setEquationAndChoices();
         }
@@ -215,7 +218,6 @@ public class StartGameActivityFragment extends Fragment {
     private void setEquationAndChoices() {
         Log.d(TAG, "**************************************");
         Log.d(TAG, "setEquationAndChoices: called");
-        Log.d(TAG, "**************************************");
 
         List<Integer> usedNumbers = new ArrayList<>();
         Random random = new Random();
@@ -228,9 +230,16 @@ public class StartGameActivityFragment extends Fragment {
         String equation = x + " + " + y;
         this.equation.setText(equation);
 
+        choices = new ArrayList<>();
+        choices.add((TextView) getView().findViewById(R.id.choice1));
+        choices.add((TextView) getView().findViewById(R.id.choice2));
+        choices.add((TextView) getView().findViewById(R.id.choice3));
+        choices.add((TextView) getView().findViewById(R.id.choice4));
+
         int correctChoice = random.nextInt(4);
         for (int i = 0; i < choices.size(); i++) {
             if (i == correctChoice) {
+                Log.d(TAG, "setEquationAndChoices: choice(" + i + ") is set to true with value = " + sum);
                 choices.get(i).setText(String.valueOf(sum));
                 choices.get(i).setTag(true);
             } else {
@@ -243,47 +252,29 @@ public class StartGameActivityFragment extends Fragment {
                 choices.get(i).setTag(false);
             }
         }
+        Log.d(TAG, "**************************************");
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        Log.d(TAG, "**************************************");
-        Log.d(TAG, "onSaveInstanceState: called");
-        Log.d(TAG, "**************************************");
-
         outState.putString(CUR_CHOICE_1, choices.get(0).getText().toString());
         outState.putString(CUR_CHOICE_2, choices.get(1).getText().toString());
         outState.putString(CUR_CHOICE_3, choices.get(2).getText().toString());
         outState.putString(CUR_CHOICE_4, choices.get(3).getText().toString());
-
-        Log.d(TAG, "onSaveInstanceState: curChoice1 = " + outState.get(CUR_CHOICE_1));
-        Log.d(TAG, "onSaveInstanceState: curChoice2 = " + outState.get(CUR_CHOICE_2));
-        Log.d(TAG, "onSaveInstanceState: curChoice3 = " + outState.get(CUR_CHOICE_3));
-        Log.d(TAG, "onSaveInstanceState: curChoice4 = " + outState.get(CUR_CHOICE_4));
 
         outState.putBoolean(CUR_CHOICE_1_TAG, (boolean) choices.get(0).getTag());
         outState.putBoolean(CUR_CHOICE_2_TAG, (boolean) choices.get(1).getTag());
         outState.putBoolean(CUR_CHOICE_3_TAG, (boolean) choices.get(2).getTag());
         outState.putBoolean(CUR_CHOICE_4_TAG, (boolean) choices.get(3).getTag());
 
-
-        Log.d(TAG, "onSaveInstanceState: curChoice1Tag = " + outState.get(CUR_CHOICE_1_TAG));
-        Log.d(TAG, "onSaveInstanceState: curChoice2Tag = " + outState.get(CUR_CHOICE_2_TAG));
-        Log.d(TAG, "onSaveInstanceState: curChoice3Tag = " + outState.get(CUR_CHOICE_3_TAG));
-        Log.d(TAG, "onSaveInstanceState: curChoice4Tag = " + outState.get(CUR_CHOICE_4_TAG));
-
-
         outState.putString(CUR_SCORE, score.getText().toString());
+        outState.putString(CUR_EQUATION, equation.getText().toString());
 
         super.onSaveInstanceState(outState);
     }
 
     @Override
     public void onDetach() {
-        Log.d(TAG, "**************************************");
-        Log.d(TAG, "onDetach: called");
-        Log.d(TAG, "**************************************");
-
         super.onDetach();
         mListener = null;
         if (mTimer != null) {
